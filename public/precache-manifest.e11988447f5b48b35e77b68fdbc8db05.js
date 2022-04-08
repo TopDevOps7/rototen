a00aa841e92317961c10c8e3e5deccb3e2e13802
TopDevOps7 <86312703+TopDevOps7@users.noreply.github.com>
2022-04-08 21:31:34 +0900

    completerototen

---
+++ public/precache-manifest.e11988447f5b48b35e77b68fdbc8db05.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = [
+  {
+    "revision": "d8e18ae7b37512b3b969",
+    "url": "/static/css/main.e52d6e2e.chunk.css"
+  },
+  {
+    "revision": "d8e18ae7b37512b3b969",
+    "url": "/static/js/main.0fe71596.chunk.js"
+  },
+  {
+    "revision": "2b517db2796b2f9355dd",
+    "url": "/static/js/runtime~main.8db79d06.js"
+  },
+  {
+    "revision": "4286efbe47f2ca52ec12",
+    "url": "/static/css/2.8b3958f8.chunk.css"
+  },
+  {
+    "revision": "4286efbe47f2ca52ec12",
+    "url": "/static/js/2.b447dbdd.chunk.js"
+  },
+  {
+    "revision": "544f18c7119458d0cefc",
+    "url": "/static/js/3.716ae79c.chunk.js"
+  },
+  {
+    "revision": "f1925822b26f67622ea523e77d1d39c5",
+    "url": "/static/media/btc.f1925822.png"
+  },
+  {
+    "revision": "4d4e6e194b92ee7807286ae2208f79d4",
+    "url": "/static/media/eth.4d4e6e19.png"
+  },
+  {
+    "revision": "e1df49b46b09163fd5b859b48965ed88",
+    "url": "/static/media/logo.e1df49b4.png"
+  },
+  {
+    "revision": "0b4ac1dc75df35e169b70d7719afe4cc",
+    "url": "/static/media/notification.0b4ac1dc.ttf"
+  },
+  {
+    "revision": "5bee74caefdf9d0a834915f6c8eeb259",
+    "url": "/static/media/notification.5bee74ca.svg"
+  },
+  {
+    "revision": "651771e1df95c807c99608188d0a4287",
+    "url": "/static/media/notification.651771e1.woff"
+  },
+  {
+    "revision": "c0d3c94cd6112550c51d7d1ed13b9da1",
+    "url": "/static/media/notification.c0d3c94c.eot"
+  },
+  {
+    "revision": "4d23f3ddd411640f28a8914415b3ba85",
+    "url": "/static/media/video1.4d23f3dd.mp4"
+  },
+  {
+    "revision": "e84960366a7fa834051f71c3db2c8390",
+    "url": "/static/media/logo-2.e8496036.png"
+  },
+  {
+    "revision": "3a97a304a57096e6afba64d5734317df",
+    "url": "/static/media/playstore.3a97a304.webp"
+  },
+  {
+    "revision": "5b133fb0a6590eb5dbe35eee543ed27e",
+    "url": "/static/media/appstore.5b133fb0.webp"
+  },
+  {
+    "revision": "d15b79e5d5802a6af2f77d04cefb4126",
+    "url": "/index.html"
+  }
+];
